5c1dec394c6c103e2c30b9d17a5d5e7c6de1c25c
wandieinnocents <wandieinnocent2@gmail.com>
2022-05-27 16:10:11 +0300

    Add view for api without key

--- app/Http/Controllers/AuthorControllerApi.php
+++ app/Http/Controllers/AuthorControllerApi.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class AuthorControllerApi extends Controller
 {
@@ -10,12 +11,42 @@ class AuthorControllerApi extends Controller
     // fetch authors without key
     public function FetchAuthorsWithoutApiKey(){
 
-        return view('authors.apiwithoutkey');
+        $client = new Client(); //GuzzleHttp\Client
+        $url = "https://api.github.com/users/kingsconsult/repos";
+
+
+        $response = $client->request('GET', $url, [
+            'verify'  => false,
+        ]);
+
+        $responseBody = json_decode($response->getBody());
+
+        return view('authors.apiwithoutkey', compact('responseBody'));
+
     }
 
     // fetch authors with key
     public function FetchAuthorsWithApiKey(){
-        return view('authors.apiwithkey');
+        $client = new Client();
+        $url = "https://dev.to/api/articles/me/published";
+
+        $params = [
+            //If you have any Params Pass here
+        ];
+
+        $headers = [
+            'api-key' => 'k3Hy5qr73QhXrmHLXhpEh6CQ'
+        ];
+
+        $response = $client->request('GET', $url, [
+            // 'json' => $params,
+            'headers' => $headers,
+            'verify'  => false,
+        ]);
+
+        $responseBody = json_decode($response->getBody());
+
+        return view('authors.apiwithkey', compact('responseBody'));
 
     }
 
