23ca97f66340aa379bf85bd6683337e1f47779f8
wandieinnocents <wandieinnocent2@gmail.com>
2022-05-27 22:08:24 +0300

    Refactor post api with json_decode

--- app/Http/Controllers/AuthorControllerApi.php
+++ app/Http/Controllers/AuthorControllerApi.php
@@ -30,30 +30,30 @@ class AuthorControllerApi extends Controller
     // Add author
     public function AddAuthor(){
 
-        // dd("wandie");
-
         $client = new Client();
         $url = 'http://localhost:8002/api/authors';
         $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDIvYXBpL2xvZ2luIiwiaWF0IjoxNjUzNjc2NjY5LCJleHAiOjE2NTM2ODAyNjksIm5iZiI6MTY1MzY3NjY2OSwianRpIjoib3k0eGpJejA4Sktxb0JOdiIsInN1YiI6IjkiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.Hj02hiuJrg5epvynpiz8SvRf388ZknWnKr7CSlvXwTA';
-
+        //pick form parameters
         $form_params = [
-            'name'                        => 'dev1',
-            'email'                       => '2john.smith@gmail.com',
+            'name'                        => 'kapere',
+            'email'                       => 'ldakaperejohn.smith@gmail.com',
             'twitter'                     => '@innocentWandie',
             'github'                      => 'wandieinnocents',
             'location'                      => 'kampala',
             'latest_article_published'    => 'There is the latest',
             
         ];
-
+        //response to post data
         $response = $client->post($url, ['form_params' => $form_params],[
             'headers' => [
                 'Authorization' => 'Bearer '.$token,
                 'Accept' => 'application/json',
             ],
            ]);
-        
-        $response = $response->getBody()->getContents();
+
+        $responseBody = json_decode($response->getBody()->getContents());
+        dd($responseBody);
+      
  
      }
 
