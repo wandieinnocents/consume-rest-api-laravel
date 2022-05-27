54e22cbe4e79280257ab1ca10f30925c3efaf77a
wandieinnocents <wandieinnocent2@gmail.com>
2022-05-27 15:49:40 +0300

    Modify auth controller

--- app/Http/Controllers/AuthorControllerApi.php
+++ app/Http/Controllers/AuthorControllerApi.php
@@ -10,11 +10,12 @@ class AuthorControllerApi extends Controller
     // fetch authors without key
     public function FetchAuthorsWithoutApiKey(){
 
-
+        return "api without key";
     }
 
     // fetch authors with key
     public function FetchAuthorsWithApiKey(){
+        return "api with key";
 
     }
 
